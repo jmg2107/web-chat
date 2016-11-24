a52ec3c4cae5103b177a490242879c7455dee940
jmg2107 <jenjengoo@gmail.com>
2016-11-24 14:56:46 -0800

    (feat) Add initial LinkedIn API code Add user IP address to backend

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,13 +1,28 @@
 var db = require('./db.js');
 var request = require('request');
+var ip = require('ip');
 var User = require('./user.js');
 var Chat = require('./chats.js');
+
+if(process.env.PORT){
+  var linkedInKey = process.env.LINKEDIN_KEY;
+  var linkedInSecret = process.env.LINKEDIN_SECRET;
+} else {
+  var linkedInKey = require('./config.js').key;
+  var linkedInSecret = require('./config.js').secret;
+}
+
+var LinkedIn = require('node-linkedin')(linkedInKey, linkedInSecret);
+
 module.exports= {
 
   createUser: function(req, res, next){
     console.log('hit createUser');
     console.log('username is ', req.body.username);
+
     var username = req.body.username;
+    var ipAddress = ip.address();
+    username = username + '.' + ipAddress;
 
     var newUser = new User({
       username: username
@@ -15,18 +30,24 @@ module.exports= {
 
     User.findOne({username: username})
       .exec(function(err, user){
-        newUser.save(function(err, newUser){
+        if(!user){
+          newUser.save(function(err, newUser){
 
-          if(err){
-            res.status(500).send(err);
-            return;
-          }
-          console.log('new user created');
-          req.session.user = username;
-          console.log('req session user is ', req.session.user);
-          res.status(201).send(newUser);
+            if(err){
+              res.status(500).send(err);
+              return;
+            }
+            console.log('new user created');
+            req.session.user = username;
+            console.log('req session user is ', req.session.user);
 
-        });
+            res.status(201).send(newUser);
+
+          });
+        } else {
+          console.log('user already exists: ', user);
+          res.status(200).send(user);
+        }
 
       });
 
